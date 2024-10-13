ca6101d5215f55ace94e6dfce2b7f54d5bf70518
Rohit Mondal <rohitmondal.22@nshm.edu.in>
2024-10-13 11:31:18 +0530

    unwanted revert

--- Backend/dist/message/outgoingMessage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OutgoingSupportedMessage = void 0;
-var OutgoingSupportedMessage;
-(function (OutgoingSupportedMessage) {
-    OutgoingSupportedMessage["AddChat"] = "ADD_CHAT";
-    OutgoingSupportedMessage["UpdateChat"] = "UPDATE_CHAT";
-})(OutgoingSupportedMessage || (exports.OutgoingSupportedMessage = OutgoingSupportedMessage = {}));
